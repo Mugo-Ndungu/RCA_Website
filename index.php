dee844d02da35e36acd0892ba269252294f9794d
Mugo-Ndungu <twinnymugo@gmail.com>
2023-09-06 14:11:57 +0300

    Initial Commit

---
+++ index.php
@@ -0,0 +1,309 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<?php
+include './header.php';
+?>
+
+<body>
+    <main></main>
+    <?php
+    include './navbar.php';
+    ?>
+    <section class="slider-area position-relative">
+        <div class="slider-active">
+
+            <div class="single-slider slider-height hero-overly slider-bg1 d-flex  align-items-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xxl-6 col-xl-7 col-lg-8 col-md-10 col-sm-12">
+                            <div class="hero-caption">
+                                <span data-animation="fadeInUp" data-delay=".2s">REACT CERT AFRICA</span>
+                                <h1 data-animation="fadeInUp" data-delay=".2s" style="text-transform: uppercase;">
+                                    Precision Audits, Powerful Results</h1>
+                                <P data-animation="fadeInUp" data-delay=".4s">Your Beacon in Systems Clarity:
+                                    Trusted Partners in Audit Excellence, Guiding Your Success with Precision and
+                                    Integrity.</P>
+                                <a href="#" class="btn_1 hero-btn" data-animation="fadeInUp" data-delay=".8s">Get
+                                    Started Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="visit-three fix">
+
+        <div class="tailor-details">
+            <div class="section-tittle mb-40">
+                <h2>RCA is on a mission to assure fairness</h2>
+                <p>Maecenas felis felis, vulputate sit amet mauris et, semper aliquam ligula.<br> Integer efficitur
+                    tellus metus, sed feugiat leo posuere. <br><i class="fa fa-dot-circle-o" aria-hidden="true"></i> Maecenas felis felis, vulputate sit amet mauris et, semper aliquam ligula. Integer efficitur
+                    tellus metus, sed feugiat leo posuere. <br><i class="fa fa-dot-circle-o" aria-hidden="true"></i> Maecenas felis felis, vulputate sit amet mauris et, semper aliquam ligula. Integer efficitur
+                    tellus metus, sed feugiat leo posuere. <br><i class="fa fa-dot-circle-o" aria-hidden="true"></i> Maecenas felis felis, vulputate sit amet mauris et, semper aliquam ligula. Integer efficitur
+                    tellus metus, sed feugiat leo posuere. <br><i class="fa fa-dot-circle-o" aria-hidden="true"></i> Maecenas felis felis, vulputate sit amet mauris et, semper aliquam ligula. Integer efficitur
+                    tellus metus, sed feugiat leo posuere. <br><i class="fa fa-dot-circle-o" aria-hidden="true"></i> Maecenas felis felis, vulputate sit amet mauris et, semper aliquam ligula. Integer efficitur
+                    tellus metus, sed feugiat leo posuere.</p>
+            </div>
+
+            <!-- <div class="single-gallery mb-15">
+                    <div class="thumb-content-box d-flex">
+                        <div class="thumb-content">
+                            <div class="capt">
+                                <h3>First year students</h3>
+                                <p>Integer efficitur tellus metus, sed feugiat leo posuere ac.<br> Morbi vitae tincidunt
+                                    mi, et malesuada massa.</p>
+                            </div>
+                            <a href="projects.php"><i class="ti-angle-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="single-gallery mb-15">
+                    <div class="thumb-content-box d-flex">
+                        <div class="thumb-content">
+                            <div class="capt">
+                                <h3>Tuition & fees</h3>
+                                <p>Integer efficitur tellus metus, sed feugiat leo posuere ac.<br> Morbi vitae tincidunt
+                                    mi, et malesuada massa.</p>
+                            </div>
+                            <a href="projects.php"><i class="ti-angle-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="single-gallery mb-15">
+                    <div class="thumb-content-box d-flex">
+                        <div class="thumb-content">
+                            <div class="capt">
+                                <h3>International students</h3>
+                                <p>Integer efficitur tellus metus, sed feugiat leo posuere ac.<br> Morbi vitae tincidunt
+                                    mi, et malesuada massa.</p>
+                            </div>
+                            <a href="projects.php"><i class="ti-angle-right"></i></a>
+                        </div>
+                    </div>
+                </div> -->
+        </div>
+
+        <!-- <div class="visit-team"></div> -->
+    </section>
+
+
+    <section class="about-area section-bg section-padding">
+        <div class="container">
+            <div class="row align-items-center">
+                <h2>Check us out in action during an audit!</h2>
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12">
+
+                    <!-- <div class="about-img about-img1  ">
+                            <img src="assets/img/gallery/about1.jpg" alt>
+                        </div> -->
+                    <div class="about-img about-img1  ">
+                        <div id="player"></div>
+                        <!-- <iframe data-gtm-yt-inspected-12="true" loading="lazy"
+                                title="Trader Audit by RCA at TeeGschwendner" width="500" height="281"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                frameborder="0" allowfullscreen src="https://youtu.be/pLa9vNyQfvI">
+                            </iframe> -->
+                    </div>
+                </div>
+                <div class="offset-xl-1 offset-lg-0 offset-sm-1 col-xxl-5 col-xl-5 col-lg-6 col-md-9 col-sm-11">
+                    <div class="about-caption about-caption1">
+
+                        <div class="section-tittle m-0">
+                            <h2>Want to learn more about the audit process?</h2>
+                            <a href="#" class="browse-btn mt-20">Visit our How to Join RCA page</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="home-blog section-padding">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-12 col-lg-12 col-md-12">
+
+                    <div class="section-tittle text-center mb-40">
+                        <h2>Our Services</h2>
+                        <p><strong class="color-danger">React Ceart Africa</strong> is committed to delivering exceptional solutions tailored to your needs. With a dedicated team of experts, cutting-edge technology, and a passion for exceeding expectations, we take pride in offering a range of services designed to propel your success. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-md-6">
+                    <div class="single-blogs mb-30">
+                        <div class="blog-img">
+                            <img src="assets/img/gallery/blog2.jpg" alt>
+                        </div>
+                        <div class="blog-caption">
+                            <h3><a href="#">Product Certification</a></h3>
+                            <p>Unlock market potential with our Product Certification service. Ensure your products meet industry standards, gain trust, and expand your reach. Trust our expertise to navigate the certification process efficiently, saving you time and resources. Elevate your products today.</p>
+                            <a href="#" class="browse-btn">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="single-blogs">
+                        <div class="blog-img">
+                            <img src="assets/img/gallery/blog1.jpg" alt>
+                        </div>
+                        <div class="blog-caption">
+                            <h3><a href="#">Trainings</a></h3>
+                            <p>Elevate your team's expertise with our Auditor Training service. Empower your professionals with industry-leading knowledge and skills. Our comprehensive programs ensure compliance, efficiency, and excellence in auditing practices. Start your journey to becoming a certified auditor today.</p>
+                            <a href="#" class="browse-btn">Learn More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="class-offer-area section-padding border-bottom">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-12">
+
+                    <div class="section-tittle  d-flex justify-content-between align-items-center">
+                        <h2>News</h2>
+                        <!-- <a href="#" class="browse-btn mb-20">More Stories</a> -->RCA
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="properties pb-30">
+                        <div class="properties__card">
+                            <div class="properties__img">
+                                <a href="#"><img src="assets/img/gallery/class-img1.jpg" alt></a>
+                            </div>
+                            <div class="properties__caption text-center">
+                                <h3><a href="#">Linguistics alumna says recognizing Indigenous Languages Day is
+                                        crucial to our histories</a></h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="properties pb-30">
+                        <div class="properties__card">
+                            <div class="properties__img">
+                                <a href="#"><img src="assets/img/gallery/class-img1.jpg" alt></a>
+                            </div>
+                            <div class="properties__caption text-center">
+                                <h3><a href="#">Linguistics alumna says recognizing Indigenous Languages Day is
+                                        crucial to our histories</a></h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="properties pb-30">
+                        <div class="properties__card">
+                            <div class="properties__img">
+                                <a href="#"><img src="assets/img/gallery/class-img1.jpg" alt></a>
+                            </div>
+                            <div class="properties__caption text-center">
+                                <h3><a href="#">Linguistics alumna says recognizing Indigenous Languages Day is
+                                        crucial to our histories</a></h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- <section class="brand-area section-padding">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-8 col-lg-8 col-md-10">
+
+                        <div class="section-tittle text-center mb-60">
+                            <h2>Our Partners</h2>
+                            <p>Maecenas felis felis, vulputate sit amet mauris et, semper aliquam ligula. Integer
+                                efficitur tellus metus, sed feugiat leo posuere.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center align-items-end">
+                    <div class="col-xl-8 col-lg-9">
+                        <div class="brand-active text-center">
+                            <div class="single-brand">
+                                <img src="assets/img/gallery/brand1.png" alt>
+                            </div>
+                            <div class="single-brand">
+                                <img src="assets/img/gallery/brand2.png" alt>
+                            </div>
+                            <div class="single-brand">
+                                <img src="assets/img/gallery/brand3.png" alt>
+                            </div>
+                            <div class="single-brand">
+                                <img src="assets/img/gallery/brand4.png" alt>
+                            </div>
+                            <div class="single-brand">
+                                <img src="assets/img/gallery/brand2.png" alt>
+                            </div>
+                            <div class="single-brand">
+                                <img src="assets/img/gallery/brand3.png" alt>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section class="visit-one fix">
+
+            <div class="visit-team">
+                <div class="wrapper"></div>
+            </div>
+
+            <div class="tailor-details">
+                <div class="section-tittle section-tittle2 mb-25">
+                    <h2>TRUSTED BY OVER <br> 6000+ STUDENTS</h2>
+                    <p class="mb-30">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas felis felis,
+                        vulputate sit amet mauris et, semper aliquam ligula. Integer efficitur tellus metus, sed feugiat
+                        leo posuere ac. Morbi vitae tincidunt malesuada massa.</p>
+                    <p class="mb-30">Maecenas felis felis, vulputate sit amet mauris et, semper aliquam ligula. Integer
+                        efficitur tellus metus, sed feugiat leo posuere ac. Morbi vitae tincidunt mi, et malesuada
+                        massa.</p>
+                    <a href="#" class="browse-btn browse-btn2 mt-20">Join Now</a>
+                </div>
+            </div>
+        </section> -->
+
+    </main>
+    <?php
+    include './footer.php';
+    ?>
+
+    <div id="back-top">
+        <a title="Go to Top" href="#"><i class="fa fa-arrow-up"></i></a>
+    </div>
+    <script>
+        const videoUrl = 'https://www.youtube.com/watch?v=pLa9vNyQfvI';
+
+        // Get the video content via the proxy server
+        fetch(`http://localhost:3000/proxy?videoUrl=${encodeURIComponent(videoUrl)}`)
+            .then(response => response.text())
+            .then(videoContent => {
+                // Inject the video content into an iframe on your webpage
+                const playerContainer = document.getElementById('player');
+                playerContainer.innerHTML = '<iframe width="500" height="281" srcdoc="${videoContent}" frameborder="0" allowfullscreen></iframe>';
+            })
+            .catch(error => console.error(error));
+    </script>
+
+    <?php
+    include './scripts.php';
+    ?>
+</body>
+
+</html>
